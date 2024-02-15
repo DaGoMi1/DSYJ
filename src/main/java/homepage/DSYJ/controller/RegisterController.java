180e6a5759bb1880ba82b9a37d2934c5abfbf0d4
DaGeom Lee <dlekrja4523@gmail.com>
2024-02-15 15:54:31 +0900

    commit

--- src/main/java/homepage/DSYJ/controller/RegisterController.java
+++ src/main/java/homepage/DSYJ/controller/RegisterController.java
@@ -3,6 +3,8 @@ package homepage.DSYJ.controller;
 import homepage.DSYJ.domain.Member;
 import homepage.DSYJ.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -36,12 +38,26 @@ public class RegisterController {
 
         memberService.sendCodeToEmail(form.getEmail());
 
-
-        if (member.getPassword().equals(password2)) {
+        if (memberService.isPasswordMatching(member.getPassword(), password2)) {
             memberService.join(member);
             return "redirect:/";
         } else {
             return "error";
+        }
+    }
+
+    @PostMapping("/emails/verification")
+    public ResponseEntity<?> verifyEmail(@RequestParam String email, @RequestParam String authCode) {
+        boolean verificationResult = memberService.verifiedCode(email, authCode);
+
+        if (verificationResult) {
+            // 이메일 인증 성공
+            // 원하는 동작 수행 (예: 회원 가입 처리)
+            return new ResponseEntity<>("이메일 인증 성공", HttpStatus.OK);
+        } else {
+            // 이메일 인증 실패
+            // 원하는 동작 수행 (예: 에러 응답)
+            return new ResponseEntity<>("이메일 인증 실패", HttpStatus.BAD_REQUEST);
         }
     }
 
