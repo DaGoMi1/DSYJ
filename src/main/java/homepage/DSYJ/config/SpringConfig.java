8c6986fd5dd724596b451f24e5af92f2ee369d3d
DaGeom Lee <dlekrja4523@gmail.com>
2024-02-15 15:54:47 +0900

    commit

--- src/main/java/homepage/DSYJ/config/SpringConfig.java
+++ src/main/java/homepage/DSYJ/config/SpringConfig.java
@@ -1,40 +1,44 @@
 package homepage.DSYJ.config;
 
-import homepage.DSYJ.repository.SpringDataJpaCommentRepository;
-import homepage.DSYJ.repository.SpringDataJpaMemberRepository;
-import homepage.DSYJ.repository.SpringDataJpaPostingRepository;
-import homepage.DSYJ.repository.SpringDataJpaScheduleRepository;
+import homepage.DSYJ.repository.*;
 import homepage.DSYJ.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
 public class SpringConfig {
-
     private final SpringDataJpaPostingRepository springDataJpaPostingRepository;
     private final SpringDataJpaMemberRepository springDataJpaMemberRepository;
     private final SpringDataJpaScheduleRepository springDataJpaScheduleRepository;
     private final SpringDataJpaCommentRepository springDataJpaCommentRepository;
+    private final AuthCodeRepository authCodeRepository;
+    private final JavaMailSender javaMailSender;
 
     @Autowired
     public SpringConfig(SpringDataJpaPostingRepository springDataJpaPostingRepository,
                         SpringDataJpaMemberRepository springDataJpaMemberRepository,
                         SpringDataJpaScheduleRepository springDataJpaScheduleRepository,
-                        SpringDataJpaCommentRepository springDataJpaCommentRepository) {
+                        SpringDataJpaCommentRepository springDataJpaCommentRepository,
+                        AuthCodeRepository authCodeRepository,
+                        JavaMailSender javaMailSender) {
         this.springDataJpaPostingRepository = springDataJpaPostingRepository;
         this.springDataJpaMemberRepository = springDataJpaMemberRepository;
         this.springDataJpaScheduleRepository = springDataJpaScheduleRepository;
         this.springDataJpaCommentRepository = springDataJpaCommentRepository;
+        this.authCodeRepository = authCodeRepository;
+        this.javaMailSender = javaMailSender;
     }
 
     @Bean
     public CommentService commentService(){
         return new CommentService(springDataJpaCommentRepository);
     }
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(springDataJpaMemberRepository);
+        return new MemberService(springDataJpaMemberRepository, mailService(), authCodeService());
     }
 
     @Bean
@@ -45,5 +49,15 @@ public class SpringConfig {
     @Bean
     public ScheduleService scheduleService() {
         return new ScheduleService(springDataJpaScheduleRepository);
+    }
+
+    @Bean
+    public AuthCodeService authCodeService(){
+        return new AuthCodeService(authCodeRepository);
+    }
+
+    @Bean
+    public MailService mailService() {
+        return new MailService(javaMailSender);
     }
 }
