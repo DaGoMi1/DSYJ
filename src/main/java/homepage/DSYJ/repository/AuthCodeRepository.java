2e4c84d40798818b0a88a85d8153a915e7f82b8f
DaGeom Lee <dlekrja4523@gmail.com>
2024-02-16 15:25:22 +0900

    commit

--- src/main/java/homepage/DSYJ/repository/AuthCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package homepage.DSYJ.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthCodeRepository extends JpaRepository<String, String> {
-    void save(String key, String authCode);
-}
