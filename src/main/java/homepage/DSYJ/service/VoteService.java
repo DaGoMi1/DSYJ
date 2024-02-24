67151ada107d840fe7f98f9582e8ee7183d25e0b
DaGeom Lee <dlekrja4523@gmail.com>
2024-02-24 15:23:45 +0900

    commit

--- src/main/java/homepage/DSYJ/service/VoteService.java
+++ src/main/java/homepage/DSYJ/service/VoteService.java
@@ -1,26 +1,39 @@
 package homepage.DSYJ.service;
 
+import homepage.DSYJ.domain.Option;
 import homepage.DSYJ.domain.Vote;
+import homepage.DSYJ.repository.SpringDataJpaOptionRepository;
 import homepage.DSYJ.repository.SpringDataJpaVoteRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class VoteService {
     private final SpringDataJpaVoteRepository voteRepository;
+    private final SpringDataJpaOptionRepository optionRepository;
 
-    public VoteService(SpringDataJpaVoteRepository voteRepository) {
+    public VoteService(SpringDataJpaVoteRepository voteRepository,
+                       SpringDataJpaOptionRepository optionRepository) {
         this.voteRepository = voteRepository;
+        this.optionRepository = optionRepository;
     }
 
-    public void createVote(String topic, LocalDateTime startDate, LocalDateTime endDate) {
-        // 투표 생성
-        Vote newVote = new Vote();
-        newVote.setTopic(topic);
-        newVote.setStartDate(startDate);
-        newVote.setEndDate(endDate);
+    public void createVote(String topic, LocalDateTime startDate,
+                           LocalDateTime endDate, List<String> options) {
+        // Vote 엔티티 생성 및 저장
+        Vote vote = new Vote();
+        vote.setTopic(topic);
+        vote.setStartDate(startDate);
+        vote.setEndDate(endDate);
+        voteRepository.save(vote);
 
-        // 투표 저장
-        voteRepository.save(newVote);
+        // Option 엔티티 생성 및 저장
+        for (String optionText : options) {
+            Option option = new Option();
+            option.setVote(vote);
+            option.setOption(optionText);
+            optionRepository.save(option);
+        }
     }
 
     public Vote getVoteById(Long id) {
