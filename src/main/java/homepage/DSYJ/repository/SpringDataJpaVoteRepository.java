5482ba018af9849db25e60774531e2b1204f74a9
DaGeom Lee <dlekrja4523@gmail.com>
2024-02-25 01:44:29 +0900

    commit

--- src/main/java/homepage/DSYJ/repository/SpringDataJpaVoteRepository.java
+++ src/main/java/homepage/DSYJ/repository/SpringDataJpaVoteRepository.java
@@ -3,6 +3,11 @@ package homepage.DSYJ.repository;
 import homepage.DSYJ.domain.Schedule;
 import homepage.DSYJ.domain.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SpringDataJpaVoteRepository extends JpaRepository<Vote, Long> {
+    @Query("SELECT DISTINCT v.topic FROM Vote v")
+    List<String> findDistinctTopics();
 }
