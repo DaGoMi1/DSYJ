ce91bafa6e2afeafb9fd79683a70b16442321926
DaGeom Lee <dlekrja4523@gmail.com>
2024-03-01 00:47:00 +0900

    commit

--- src/main/java/homepage/DSYJ/service/VoteService.java
+++ src/main/java/homepage/DSYJ/service/VoteService.java
@@ -1,45 +1,12 @@
 package homepage.DSYJ.service;
 
-import homepage.DSYJ.domain.Option;
-import homepage.DSYJ.domain.Vote;
-import homepage.DSYJ.repository.SpringDataJpaOptionRepository;
 import homepage.DSYJ.repository.SpringDataJpaVoteRepository;
-import jakarta.transaction.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class VoteService {
     private final SpringDataJpaVoteRepository voteRepository;
-    private final SpringDataJpaOptionRepository optionRepository;
 
-    public VoteService(SpringDataJpaVoteRepository voteRepository,
-                       SpringDataJpaOptionRepository optionRepository) {
+    public VoteService(SpringDataJpaVoteRepository voteRepository) {
         this.voteRepository = voteRepository;
-        this.optionRepository = optionRepository;
     }
 
-    @Transactional
-    public void createVote(String topic, LocalDateTime startDate,
-                           LocalDateTime endDate, List<String> options) {
-        // Vote 엔티티 생성 및 저장
-        Vote vote = new Vote();
-        vote.setTopic(topic);
-        vote.setStartDate(startDate);
-        vote.setEndDate(endDate);
-        voteRepository.save(vote);
-
-        // Option 엔티티 생성 및 저장
-        for (String optionText : options) {
-            Option option = new Option();
-            option.setVote(vote);
-            option.setOption(optionText);
-            optionRepository.save(option);
-        }
-    }
-
-    @Transactional
-    public List<String> findDistinctTopics() {
-        return voteRepository.findDistinctTopics();
-    }
 }
