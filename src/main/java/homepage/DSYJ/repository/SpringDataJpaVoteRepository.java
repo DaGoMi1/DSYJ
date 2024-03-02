795a72bf25172580b6a87f3a527b1f59596bfa12
DaGeom Lee <dlekrja4523@gmail.com>
2024-03-02 15:46:35 +0900

    commit

--- src/main/java/homepage/DSYJ/repository/SpringDataJpaVoteRepository.java
+++ src/main/java/homepage/DSYJ/repository/SpringDataJpaVoteRepository.java
@@ -5,7 +5,9 @@ import homepage.DSYJ.domain.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SpringDataJpaVoteRepository extends JpaRepository<Vote, Long> {
+    List<Vote> findByStartDateBeforeAndEndDateAfter(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
