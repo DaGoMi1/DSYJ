eb702c9fa29f2b9995040267a32af7518b4d616d
DaGeom Lee <dlekrja4523@gmail.com>
2024-03-02 15:46:54 +0900

    commit

--- src/main/java/homepage/DSYJ/service/VoteService.java
+++ src/main/java/homepage/DSYJ/service/VoteService.java
@@ -1,12 +1,30 @@
 package homepage.DSYJ.service;
 
+import homepage.DSYJ.domain.Vote;
 import homepage.DSYJ.repository.SpringDataJpaVoteRepository;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class VoteService {
     private final SpringDataJpaVoteRepository voteRepository;
 
     public VoteService(SpringDataJpaVoteRepository voteRepository) {
         this.voteRepository = voteRepository;
+    }
+
+    public void saveVote(String topic, LocalDateTime startDateTime, LocalDateTime endDateTime){
+        Vote vote = new Vote();
+        vote.setTopic(topic);
+        vote.setStartDate(startDateTime);
+        vote.setEndDate(endDateTime);
+        voteRepository.save(vote);
+    }
+
+    public List<Vote> findVotes() {
+        LocalDateTime currentTime = LocalDateTime.now();
+        return voteRepository.findByStartDateBeforeAndEndDateAfter(currentTime, currentTime);
     }
 
 }
